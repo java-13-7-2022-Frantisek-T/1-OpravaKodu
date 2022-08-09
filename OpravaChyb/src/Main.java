23dfdeb65bb0ae7e484c7e981d175024ede40403
java-13-7-2022-Frantisek-T <ift_2020@volny.cz>
2022-08-09 21:59:15 +0200

    Oprava chyb ve zdrojovém kódu
    Domácí úkol z první lekce
    
    Oprava souboru .gitignore
    
    Doplnění .gitignore

--- OpravaChyb/src/Main.java
+++ OpravaChyb/src/Main.java
@@ -7,7 +7,6 @@ public class Main {
        System.out.println("Oprav chyby a spusť všechny metody!");
        System.out.println("Komentáře odstraníš klávesovou zkratkou <Ctrl>+</> - použij lomítko na numerické klávesnici.");
 
-
         OpravaKodu.ukol1();
         OpravaKodu.ukol2();
         OpravaKodu.ukol3();
